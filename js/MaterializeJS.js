112c4dee4a410bdbd76184d56943e410c19b5b1a
Joshua Hekimian <josh4amd@gmail.com>
2015-03-03 13:11:42 -0500

    Added button customizations and panel cards.

--- js/MaterializeJS.js
+++ js/MaterializeJS.js
@@ -18,6 +18,23 @@ function (id, title, image, info) {
 	return res;
 };
 
+MaterializeJS.plainPanelCardHtml =
+function (info) {
+      var res = "<div class='card-panel'>" +
+          "<span class=" + _emptyStringIfNull(info.span_classes) + ">" + _emptyStringIfNull(info.text) + " </span>" +
+        "</div>" +
+      "</div>";
+      return res;
+};
+
+jQuery.fn.addPlainPanelCard = function(info) {
+    var o = $(this[0]); // It's your element
+    newElement = $($.parseHTML(MaterializeJS.plainPanelCardHtml(info)));
+    
+    newElement.addClass(_emptyStringIfNull(info.classes));
+    o.append(newElement);
+};
+
 MaterializeJS.imageCardHtml =
 function (id, title, image, info) {
 	var res = "<div id='" + id + "' class='card blue-grey lighten-5 animated fadeInUp waves-effect waves-block waves-light'>\n" + "    <div class='card-image'>\n" + ( image ? "        <img src='" + image + "'><hr style='margin: 0;'>\n" : "") + "    </div>\n" + "    <div class='card-content'>\n" + "        <span class='card-title grey-text text-darken-4'>" + title + "</span>\n" + "        <p>" + info + "</p>\n" + "    </div>\n" +
@@ -36,14 +53,33 @@ function (info) {
 		toast("<b class='red-text'>Error:</b> MaterializeJS.buttonHtml - info must be an object!", 2500);
 		return;
 	}
-	var res = "<a " + _emptyStringIfNull(info.htmlAttributes) + ">" + _emptyStringIfNull(info.text) + "</a>";
+	var res = "<a " + _emptyStringIfNull(info.htmlAttributes) + ">" + (info.icon_classes == null ? "" : "<i class=\"" + info.icon_classes + "\"></i>" ) + _emptyStringIfNull(info.text) + "</a>";
 	return res;
 };
 
-jQuery.fn.addButton = function(info) {
+MaterializeJS.largeButtonHtml =
+function (info) {
+	if(!info) info = {};
+	if(typeof info != "object"){
+		toast("<b class='red-text'>Error:</b> MaterializeJS.buttonHtml - info must be an object!", 2500);
+		return;
+	}
+	var res = "<a " + _emptyStringIfNull(info.htmlAttributes) + ">" + (info.icon_classes == null ? "" : "<i class=\"" + info.icon_classes + "\"></i>" ) + _emptyStringIfNull(info.text) + "</a>";
+	return res;
+};
+
+jQuery.fn.addButton = function(info, isLarge) {
     var o = $(this[0]); // It's your element
     newElement = $($.parseHTML(MaterializeJS.buttonHtml(info)));
-    newElement.addClass("btn");
+    
+    if(!isLarge) {
+    	newElement.addClass("btn");
+    } else {
+    	newElement.addClass("btn-large");
+    }
+    
     newElement.addClass(_emptyStringIfNull(info.classes));
+    newElement.attr("onclick",_emptyStringIfNull(info.onClick));
+    newElement.width(info.width);
     o.append(newElement);
 };
